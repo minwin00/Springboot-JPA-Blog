77feec79e8a0d70e07481ba45e18cffa97261560
minwin00 <rst0272@naver.com>
2024-09-03 21:25:55 +0900

    27강 완료

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,10 +1,16 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import org.hibernate.query.SortDirection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +29,23 @@ public class DummyControllerTest {
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
 	
+	//http://localhost:8000/blog/dummy/user
+	@GetMapping("dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+//		if(pagingUser.isLast()) {
+//			
+//		}
+		List<User> users = pagingUser.getContent(); 
+		return users;
+	}
+	
 	//http://localhost:8000/blog/dummy/user/3
 	@GetMapping("/dummy/user/{id}")
 	public User detail(@PathVariable int id) {
@@ -33,6 +56,7 @@ public class DummyControllerTest {
 				return new IllegalArgumentException("해당 유저는 없습니다. id : "+id);
 			}
 		});
+		//user 객체 = 자바 오브젝트
 		return user;
 	}
 	
